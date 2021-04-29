9f23227985da050830bb061fc8c34aa48566df2d
Bakyt <djumabaevb@gmail.com>
2021-04-29 17:32:02 +0600

    intent created done

--- app/src/main/java/com/example/quizappkotlin/QuizQuestionsActivity.kt
+++ app/src/main/java/com/example/quizappkotlin/QuizQuestionsActivity.kt
@@ -2,13 +2,19 @@ package com.example.quizappkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.quizappkotlin.databinding.ActivityQuizQuestionsBinding
 
 class QuizQuestionsActivity : AppCompatActivity() {
     lateinit var binding2: ActivityQuizQuestionsBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding2 = ActivityQuizQuestionsBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_quiz_questions)
+        setContentView(binding2.root)
+
+        val questionsList = Constants.getQuestions()
+        Log.i("Questions"," ${questionsList.size}")
+
     }
 }

5c35858d8fd30986dff59311b838c0d1d62483d3
Bakyt <djumabaevb@gmail.com>
2021-04-29 15:50:14 +0600

    new activity created

--- app/src/main/java/com/example/quizappkotlin/MainActivity.kt
+++ app/src/main/java/com/example/quizappkotlin/MainActivity.kt
@@ -1,13 +1,22 @@
 package com.example.quizappkotlin
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.setDecorFitsSystemWindows(false)
+        } else {
+            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+        }
 
+        
     }
 }

0bed227a2d1496467dd2919dd9493dcdc91a8fa4
Bakyt <djumabaevb@gmail.com>
2021-04-29 16:36:57 +0600

    intent created

--- app/src/main/java/com/example/quizappkotlin/MainActivity.kt
+++ app/src/main/java/com/example/quizappkotlin/MainActivity.kt
@@ -1,15 +1,21 @@
 package com.example.quizappkotlin
 
+import android.content.Intent
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.WindowManager
+import android.widget.Toast
+import com.example.quizappkotlin.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
             window.setDecorFitsSystemWindows(false)
@@ -17,6 +23,14 @@ class MainActivity : AppCompatActivity() {
             window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
         }
 
-        
+        binding.btnStart.setOnClickListener {
+            if(binding.etName.text.toString().isEmpty()) {
+                Toast.makeText(this, "Please enter your name", Toast.LENGTH_SHORT).show()
+            } else {
+                val intent = Intent(this, QuizQuestionsActivity:: class.java)
+                startActivity(intent)
+                finish()
+            }
+        }
     }
 }
--- app/src/main/java/com/example/quizappkotlin/QuizQuestionsActivity.kt
+++ app/src/main/java/com/example/quizappkotlin/QuizQuestionsActivity.kt
@@ -2,8 +2,11 @@ package com.example.quizappkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.quizappkotlin.databinding.ActivityQuizQuestionsBinding
 
 class QuizQuestionsActivity : AppCompatActivity() {
+    lateinit var binding2: ActivityQuizQuestionsBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz_questions)

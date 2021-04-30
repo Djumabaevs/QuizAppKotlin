b48c21b18d98d06c1bb427caee03e4a6fb36d654
Bakyt <djumabaevb@gmail.com>
2021-04-30 13:31:19 +0600

    fun setQuestion()

--- app/src/main/java/com/example/quizappkotlin/QuizQuestionsActivity.kt
+++ app/src/main/java/com/example/quizappkotlin/QuizQuestionsActivity.kt
@@ -7,27 +7,32 @@ import com.example.quizappkotlin.databinding.ActivityQuizQuestionsBinding
 
 class QuizQuestionsActivity : AppCompatActivity() {
     lateinit var binding2: ActivityQuizQuestionsBinding
+    private var mCurrentPosition: Int = 1
+    private var mQuestionsList: ArrayList<Question>? = null
+    private var mSelectedOptionPosition: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         binding2 = ActivityQuizQuestionsBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding2.root)
 
-        val questionsList = Constants.getQuestions()
-        Log.i("Questions"," ${questionsList.size}")
+        mQuestionsList = Constants.getQuestions()
 
-        val currentPosition = 1
-        val question: Question? = questionsList[currentPosition - 1]
+        setQuestion()
 
-        binding2.progressBar.progress = currentPosition
-        binding2.tvProgress.text = "$currentPosition" + "/" + binding2.progressBar.max
+    }
+
+    private fun setQuestion() {
+        mCurrentPosition = 1
+        val question = mQuestionsList!![mCurrentPosition - 1]
+
+        binding2.progressBar.progress = mCurrentPosition
+        binding2.tvProgress.text = "$mCurrentPosition" + "/" + binding2.progressBar.max
         binding2.tvQuestion.text = question!!.question
         binding2.ivImage.setImageResource(question.image)
         binding2.tvOptionOne.text = question.optionOne
         binding2.tvOptionTwo.text = question.optionTwo
         binding2.tvOptionThree.text = question.optionThree
         binding2.tvOptionFour.text = question.optionFour
-
-
     }
 }

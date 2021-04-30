cb999326a7e6d5907ae985fb002be7d2a4ecf169
Bakyt <djumabaevb@gmail.com>
2021-04-30 13:06:01 +0600

    binding2 -> progressBar

--- app/src/main/java/com/example/quizappkotlin/QuizQuestionsActivity.kt
+++ app/src/main/java/com/example/quizappkotlin/QuizQuestionsActivity.kt
@@ -16,5 +16,18 @@ class QuizQuestionsActivity : AppCompatActivity() {
         val questionsList = Constants.getQuestions()
         Log.i("Questions"," ${questionsList.size}")
 
+        val currentPosition = 1
+        val question: Question? = questionsList[currentPosition - 1]
+
+        binding2.progressBar.progress = currentPosition
+        binding2.tvProgress.text = "$currentPosition" + "/" + binding2.progressBar.max
+        binding2.tvQuestion.text = question!!.question
+        binding2.ivImage.setImageResource(question.image)
+        binding2.tvOptionOne.text = question.optionOne
+        binding2.tvOptionTwo.text = question.optionTwo
+        binding2.tvOptionThree.text = question.optionThree
+        binding2.tvOptionFour.text = question.optionFour
+
+
     }
 }
